da95911c5f9e43c1a0f604313c51b4bfa21d6702
ajibolaadam640-pixel <ajibola.adam640@gmail.com>
2026-08-12 08:01:25 +0100

    Create data.ts

---
+++ app/data.ts
@@ -0,0 +1,10 @@
+export const videos = [
+  {
+    id: 1,
+    videoUrl: '/videos/dance.mp4',
+    poster: '/posters/dance.png',
+    caption: 'Check my new video! 🔥',
+    hashtags: ['new', 'trending', 'fyp'],
+    song: 'Original Sound',
+  }
+]
