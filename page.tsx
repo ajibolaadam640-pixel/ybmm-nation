d60509c3afb1e8a19ca10a0f8047b5afcba15a5a
ajibolaadam640-pixel <ajibola.adam640@gmail.com>
2026-08-12 13:16:10 +0100

    Create page.tsx

---
+++ page.tsx
@@ -0,0 +1,14 @@
+export default function Home() {
+  return (
+    <main style={{
+      display: 'flex', 
+      justifyContent: 'center', 
+      alignItems: 'center', 
+      height: '100vh',
+      background: 'black',
+      color: 'white'
+    }}>
+      <h1>YBMM Nation 🚀</h1>
+    </main>
+  )
+}
